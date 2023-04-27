9ec4c37e6002d7176940f44b1c99620c07eceb0c
EziKielCroat <alahairlines@gmail.com>
2023-04-28 00:37:36 +0200

    react router login/register pocet

---
+++ src/Components/Home.tsx
@@ -0,0 +1,22 @@
+
+
+function Home() {
+    return(
+        <div>
+            <div className="container">
+                <nav className="nav-holder">
+                    <h2>Azil za životinje</h2>
+
+                    <ul className="nav-stranice">
+                        <button><li>O nama</li></button>
+                        <button><li>Donacije</li></button>
+                        <button><li>Obavijesti</li></button>
+                        <button><li>Admin opcije</li></button>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Components/Login.tsx
@@ -0,0 +1,11 @@
+
+function Login () {
+
+    return(
+        <div className="container">
+            <h2>Login page</h2>
+        </div>
+    )
+}
+// Stiliziraj page, napravi firebase authentication kako zapravo triba
+export default Login
